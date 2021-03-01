5a9323e39670f7c261d951db773aad22561a0724
kushal sharma <kushal0074@users.noreply.github.com>
2021-03-01 19:58:53 +0530

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontEndAssesment';
+  registerForm: FormGroup;
+    loading = false;
+    submitted = false;
+    error: string;
+
+    form = new FormGroup({
+      firstName : new FormControl('' , Validators.required),
+      lastName : new FormControl('', Validators.required),
+      email : new FormControl('' , Validators.required),
+      userName : new FormControl('' , Validators.required),
+      password : new FormControl('' , Validators.required),
+      confirmPassword : new FormControl('' , Validators.required),
+      phone : new FormControl('', Validators.required),
+      goverment: new FormControl('', Validators.required)
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  }
+  onSubmit(){
+    if(this.registerForm.valid){
+      alert('Login Successfull')
+    } else {
+      alert('User form is not valid!!')
+    }
+  }
+  get FirstName(){
+    return this.form.get('firstName');
+  }
+  get LastName(){
+    return this.form.get('lastName');
+  }
+  get Email(){
+      return this.form.get('email');
+  }
+  get UserName(){
+    return this.form.get('userName')
+  }
+  get Password(){
+      return this.form.get('password');
+  }
+  get ConfirmPassword(){
+      return this.form.get('confirmPassword');
+  }
+  get Phone()
+  {
+    return this.form.get('phone');
+  }
+  Register()
+  {
+    alert('Registered ' + this.FirstName.value +'  '+ this.LastName.value);
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
